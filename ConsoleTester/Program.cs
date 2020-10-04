6e1f7fb8b2049e416254b297ed4f4f1345b23d86
oleeriksen <oe@easv.dk>
2020-10-04 21:36:41 +0200

    file reader added

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using ProductionCode;
+
+namespace ConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovieRepository rep = new MovieRepositoryFileReader();
+            List<BERating> ratings = (List<BERating>)rep.GetAll();
+            for (int i = 0; i < 10; i++)
+            {
+                BERating c = ratings[i];
+                Console.WriteLine("Reviewer = " + c.Reviewer);
+            }
+        }
+    }
+}
---
+++ ProductionCode/MovieRepositoryFileReader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ProductionCode
+{
+    public class MovieRepositoryFileReader : IMovieRepository
+    {
+        private readonly string _path = "ratings.json";
+
+        public MovieRepositoryFileReader()
+        {
+            GetReviewsFromFile(_path);
+        }
+
+        private IEnumerable<BERating> _ratingCollection;
+
+        public IEnumerable<BERating> GetAll()
+        {
+            return _ratingCollection;
+        }
+
+        public void GetReviewsFromFile(string _path)
+        {
+            using (StreamReader streamReader = File.OpenText(_path))
+            using (JsonTextReader reader = new JsonTextReader(streamReader))
+            {
+                reader.CloseInput = true;
+                var serializer = new JsonSerializer();
+                var ratings = new List<BERating>();
+
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonToken.StartObject)
+                    {
+                        BERating review = serializer.Deserialize<BERating>(reader);
+                        ratings.Add(review);
+                    }
+
+                }
+                _ratingCollection = ratings;
+            }
+        }
+    }
+}
