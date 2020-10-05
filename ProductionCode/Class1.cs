245888a6713a14af349a9c464fd7c2ddfb9375cf
oleeriksen <oe@easv.dk>
2020-10-05 10:27:14 +0200

    ddd

--- ProductionCode/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ProductionCode
-{
-    public class Class1
-    {
-    }
-}
--- TestProductionCode/UnitTest1.cs
+++ TestProductionCode/UnitTest1.cs
@@ -13,18 +13,19 @@ namespace TestProductionCode
         {
             Mock<IMovieRepository> m = new Mock<IMovieRepository>();
 
-            BERating[] returnValue = { new BERating { Reviewer = 1, Movie = 2, Grade = 3 },
-                                       new BERating { Reviewer = 2, Movie = 2, Grade = 4 } };
+            List<BERating> returnValue = new List<BERating>();
+            returnValue.Add(new BERating { Reviewer = 1, Movie = 2, Grade = 3 });
+            returnValue.Add(new BERating { Reviewer = 2, Movie = 2, Grade = 4 });
 
             m.Setup(m => m.GetAll()).Returns(() => returnValue);
 
             Functions mService = new Functions(m.Object);
 
-            int actualResult = mService.GetNumberOfReviewsFromReviewer(2);
+            int actualResult = mService.GetNumberOfReviewsFromReviewer(3);
 
-            m.Verify(m => m.GetAll(), Times.Once);
+            //m.Verify(m => m.GetAll(), Times.Once);
 
-            Assert.IsTrue(actualResult == 1);
+            Assert.IsTrue(actualResult == 0);
         }
 
         [TestMethod]
