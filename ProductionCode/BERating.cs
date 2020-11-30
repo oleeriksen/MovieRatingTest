722c26e94ad1c35595bea6a28d5f6f63b64be702
oleeriksen <oe@easv.dk>
2020-11-30 09:16:16 +0100

    Update BERating.cs

--- ProductionCode/BERating.cs
+++ ProductionCode/BERating.cs
@@ -3,12 +3,11 @@ namespace ProductionCode
 {
     public class BERating
     {
-        public BERating()
-        {}
-
+        
         public int Reviewer { get; set; }
         public int Movie { get; set; }
         public int Grade { get; set; }
         public DateTime Date { get; set; }
+
     }
 }
